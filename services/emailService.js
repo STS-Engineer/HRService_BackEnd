22da61d3a4a191afb410b24e9d471bffe529b571
STS-Engineer <administration.sts@avocarbon.com>
2024-12-12 17:17:48 +0100

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -4,7 +4,7 @@ const pool = require("../config/database");
 
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.office365.com",
+  host: "smtp.mail.outlook.com",
   port: 587,
   secure: false,
   auth: {
