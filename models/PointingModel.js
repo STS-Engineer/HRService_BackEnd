e0311b4eb626286a03c76c25744f08a1cecbdb65
STS-Engineer <administration.sts@avocarbon.com>
2025-05-12 11:13:22 +0100

    Update PointingModel.js

--- models/PointingModel.js
+++ models/PointingModel.js
@@ -39,7 +39,7 @@ const getPointingLogs = async ({ employeeId, startDate, endDate }) => {
   const whereClause = filters.length ? `WHERE ${filters.join(" AND ")}` : "";
 
   const query = `
-    SELECT 
+     SELECT 
       p.*, 
       u.firstname, 
       u.lastname,
@@ -48,7 +48,6 @@ const getPointingLogs = async ({ employeeId, startDate, endDate }) => {
         THEN 'Late' 
         ELSE 'On Time' 
       END AS late_status
-    SELECT p.*, u.firstname, u.lastname
     FROM pointing p
     INNER JOIN users u ON p.employee_id = u.id
     ${whereClause}
